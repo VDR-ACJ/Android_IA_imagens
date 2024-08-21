11abd049fa9dfc52dbc313d5b5c853eb16c776d8
VALDEIR <valdeir_014@hotmail.com>
2024-08-20 21:22:58 -0300

    Versao 1 nivel de confidencia dos labels

---
+++ app/src/main/java/com/br/alura/galeria/navigation/graphs/ImageDetailGraph.kt
@@ -0,0 +1,68 @@
+package com.br.alura.galeria.navigation.graphs
+
+import android.graphics.BitmapFactory
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.br.alura.galeria.R
+import com.br.alura.galeria.navigation.Destinations
+import com.br.alura.galeria.ui.imageDetail.ImageDetailScreen
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.label.ImageLabeling
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions
+
+
+fun NavGraphBuilder.imageDetailGraph() {
+    composable(
+        route = Destinations.ImageDetail.route
+    ) {
+        val context = LocalContext.current
+
+        var description by remember {
+            mutableStateOf("#descrição #da #imagem")
+        }
+
+        val imageBitmap = BitmapFactory.decodeResource(
+            context.resources,
+            R.drawable.image_test
+        )
+
+        var currentImage: Any by remember {
+            mutableStateOf(imageBitmap)
+        }
+
+
+       // val image = InputImage.fromBitmap(imageBitmap,0)
+
+        ImageDetailScreen(
+            defaultImage = currentImage,
+            description = description,
+            onImageChange = {
+                currentImage = it
+
+                val image = InputImage.fromFilePath(context, it)
+                //ADD - 18 ago 2024
+
+
+                val labeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS)
+
+                labeler.process(image).addOnSuccessListener { labels ->
+                    labels.forEach{
+                        val labelAndConfidence = "${it.text} - ${it.confidence}"
+                        Log.d("ImageDetailScreen", labelAndConfidence)
+
+                        //description = labels.toString()
+                        description = labels.map{it.text}.toString()
+
+                    }
+
+                }
+            }
+        )
+    }
+}
